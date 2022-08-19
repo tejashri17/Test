cafa200c122ce14e8654c537f04c9d180175f22b
Tejashri Sutar <tejashrisutar121@gmail.com>
2022-08-19 17:10:39 +0530

    Add function

--- hello.java
+++ hello.java
@@ -1,7 +1,17 @@
 public class hello
 {
+public void add(int 1,int b)
+{
+addition=a+b;
+System.out.println("addition is"+ addition);
+
+}
+
 public static void main(String []args)
 {
 System.out.println("Hello");
+add(10,20);
+
+
 }
 }
